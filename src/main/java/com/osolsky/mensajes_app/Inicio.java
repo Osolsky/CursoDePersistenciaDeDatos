153055a7bf29a22fff214b7c13aa75f42c37e434
Yael Alfonseca <said_alfonseca@yahoo.com.mx>
2022-12-05 22:56:52 -0600

    Inicio de la aplicacion

---
+++ src/main/java/com/osolsky/mensajes_app/Inicio.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.osolsky.mensajes_app;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author said_
+ */
+public class Inicio {
+
+    public static void main(String[] args) {
+        Conexion conexion = new Conexion();
+        
+        try (Connection cnx = conexion.get_connection()){
+            
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
